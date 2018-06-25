ab7d77e042696393bd7a2cce010ce8ffddb88fc1
abprash <panuradh@buffalo.edu>
2018-06-24 17:48:10 -0700

    added my own algos and tested them

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -3,6 +3,20 @@
 import matplotlib.pyplot as plt
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
+import numpy
+
+#import required modules
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
+"""
+1. We have the option to try out 3 different ML algorithms - k-nn, adaboost and random forest
+2. We will try all 3, and compare the accuracy with respect to each other
+3. Lets code!
+"""
+
 
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
@@ -28,17 +42,35 @@ plt.show()
 ################################################################################
 
 
-### your code here!  name your classifier object clf if you want the 
+### your code here!  name your classifier object clf if you want the
 ### visualization code (prettyPicture) to show you the decision boundary
+def knn():
+    classifier = KNeighborsClassifier()
+    classifier.fit(features_train, labels_train)
+    predicted = classifier.predict(features_test)
+    print "K Nearest Neighbors = " + str(accuracy_score(labels_test, predicted))
+    return classifier
+
+def random_forest():
+    classifier = RandomForestClassifier()
+    classifier.fit(features_train, labels_train)
+    predicted = classifier.predict(features_test)
+    print "Random Forest Accuracy = " + str(accuracy_score(labels_test, predicted))
+    return classifier
+
+def adaboost():
+    classifier = AdaBoostClassifier()
+    classifier.fit(features_train, labels_train)
+    predicted = classifier.predict(features_test)
+    print "AdaBoost Accuracy = " + str(accuracy_score(labels_test, predicted))
+    return classifier
 
 
-
-
-
-
-
-
-try:
-    prettyPicture(clf, features_test, labels_test)
-except NameError:
-    pass
+if __name__ == "__main__":
+    try:
+        # clf = knn()
+        # clf = adaboost()
+        clf = random_forest()
+        prettyPicture(clf, features_test, labels_test)
+    except NameError:
+        pass
